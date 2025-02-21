86b5b53aa90f86c08284ff557df1acdd851b6d64
Teslim Adekoya <teslimadekoya11@gmail.com>
2025-02-21 11:08:04 +0100

    fourth commit

--- voting/managers.py
+++ voting/managers.py
@@ -4,7 +4,7 @@ class VoteManager(models.Manager):
     def get_votes_for_candidate_in_election(self, candidate, election):
         return self.filter(candidate=candidate, election=election).count()
 
-    def get_user_vote_for_election(self, user, election):
+    def check_user_vote_for_election(self, user, election):
         return self.filter(voter=user, election=election).first()  # Returns the vote if exists
 
 # Now you can use this manager in the Vote model
